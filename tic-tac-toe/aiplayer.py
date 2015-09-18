1d3cddbb99efc0e74ab4f0132ca813823cf4af7e
Aaron Chan <aaronchan@byu764064wks.app.byu.edu>
2015-09-18 13:00:09 -0600

    Added player capabilites to the tic tac toe

--- tic-tac-toe/aiplayer.py
+++ tic-tac-toe/aiplayer.py
@@ -2,13 +2,14 @@ from random import random;
 from random import randint;
 from copy import deepcopy;
 class AI_Player:
-    def __init__(self, rewardFcn, playNum):
+    def __init__(self, playNum):
         self.alpha = 0.5;
         self.gamma = 0.9;
         self.epsilon = 0.1;
         self.playNum = playNum;
         self.q = {};
-        self.rewardFcn = rewardFcn;
+        self.thisState = None;
+        self.thisAction = None;
 
     def getActions(self, board):
         actions = [];
@@ -26,26 +27,38 @@ class AI_Player:
             mag = max(abs(min(Q)), abs(maxQ));
             Q = [self.q.get((state, a), 0.0) + random() * mag - 0.5*mag
                      for i in actions];
-            
+            maxQ = max(Q);
+
+        
         best = [actions[i] for i in range(0, len(Q)) if Q[i] == maxQ];
-        pick = best[randint(0, len(best) - 1)];
+        if len(best) == 0:
+            print state;
+            print actions;
+            print maxQ
+            print "OH NO";
+        pick = best[0] if len(best) <= 1 else best[randint(0, len(best) - 1)];
 
         return pick;
 
     def act (self, board):
         actions = self.getActions(board);
         state = tuple([tuple(i) for i in board]);
-        thisState = state;
-        thisAction = self.chooseAction(state, actions);
-        boardCopy = deepcopy(board);
-        boardCopy[thisAction[0]][thisAction[1]] = self.playNum;
+        self.thisState = state;
+        self.thisAction = self.chooseAction(state, actions);
+        boardCopy = [list(t) for t in self.thisState];
+        boardCopy[self.thisAction[0]][self.thisAction[1]] = self.playNum;
+        return boardCopy;
+
+    def learn(self, reward):
+        boardCopy = [list(t) for t in self.thisState];
+        boardCopy[self.thisAction[0]][self.thisAction[1]] = self.playNum;
         nextState = tuple([tuple(i) for i in boardCopy]);
         nextActions = self.getActions(boardCopy);
 
-        Qsa = self.q.get((thisState, thisAction), 0);
-        rewardTerm = self.rewardFcn(boardCopy, self.playNum);
-        print "I got reward ", rewardTerm;
-        gammaTerm = self.gamma * max([self.q.get((nextState, a), 0) for a in nextActions]);
-        self.q[(thisState, thisAction)] = Qsa + self.alpha*(rewardTerm + gammaTerm - Qsa);
-        return boardCopy;
+        Qsa = self.q.get((self.thisState, self.thisAction), 0);
+        futureQ = [self.q.get((nextState, a), 0) for a in nextActions]
+        if len(futureQ) == 0:
+            futureQ = [0];
+        gammaTerm = self.gamma * max(futureQ);
+        self.q[(self.thisState, self.thisAction)] = Qsa + self.alpha*(reward + gammaTerm - Qsa);
                                      
--- tic-tac-toe/tictactoe-qlearn.py
+++ tic-tac-toe/tictactoe-qlearn.py
@@ -1,9 +1,5 @@
 from aiplayer import AI_Player;
 
-
-board = [[0 for i in range(0, 3)] for j in range(0, 3)];
-REWARD_TERM = 50;
-
 def getReward (board, playNum):
     for i in range(0, len(board)):
         if max(board[i]) == min(board[i]) and max(board[i]) != 0:
@@ -12,24 +8,175 @@ def getReward (board, playNum):
             return REWARD_TERM if board[0][i] == playNum else -REWARD_TERM;
     if board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[0][0] != 0:
         return REWARD_TERM if board[0][0] == playNum else -REWARD_TERM;
-    if board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[0][0] != 0:
-        return REWARD_TERM if board[0][0] == playNum else -REWARD_TERM;
+    if board[0][2] == board[1][1] and board[1][1] == board[2][0] and board[0][2] != 0:
+        return REWARD_TERM if board[0][2] == playNum else -REWARD_TERM;
     return 0;
 
-def aiMakeMove (ai):
-    board = ai.act(board);
-    return (board, getReward(board, ai.playNum) != 0);
-        
+def isTie (board):
+    for i in board:
+        for j in i:
+            if j == 0:
+                return False;
+    return True;
 
-ais = [AI_Player(getReward, 1), AI_Player(getReward, 2)];
+def inBounds (p, a, b, w, h):
+    return p[0] > a and p[0] < a + w and p[1] > b and p[1] < b + h;
+def aiMakeMove (ai):
+    res = ai.act(board);
+    return (res, getReward(res, ai.playNum) != 0);
+        
+GRAPHICAL_DISPLAY = True;
+USER_INPUT = False;
+USER_CAN_PLAY = False;
+TILE_SIZE = 100;
+REWARD_TERM = 50;
+
+board = [[0 for i in range(0, 3)] for j in range(0, 3)];
+ais = [AI_Player(1), AI_Player(2)];
+numWins = [0 for i in range(0, len(ais))];
+numTies = 0;
 cnt = 0;
-for i in range(0, 9):
-    print "------------"
+
+if not GRAPHICAL_DISPLAY:
+    for i in range(0, 400000):
+        res = aiMakeMove(ais[cnt]);
+        board = res[0];
+        if res[1]:
+            numWins[cnt] += 1;
+            board = [[0 for i in range(0, 3)] for j in range(0, 3)];
+            ais[cnt].learn(100);
+            for i in range(0, len(ais)):
+                if i != cnt:
+                    ais[i].learn(-100);
+        if isTie(board):
+            board = [[0 for i in range(0, 3)] for j in range(0, 3)];
+            numTies += 1;
+        cnt = (cnt + 1) % len(ais);
+    GRAPHICAL_DISPLAY = True;
+if GRAPHICAL_DISPLAY:
+    import sys, pygame;
+    pygame.init();
+    screen = pygame.display.set_mode((800, 600));
+    done = False;
+    reset = False;
     
-    board = ai1.act(board);
-    for b in board:
-        print b;
-    if (getReward(board, ai1.playNum)):
-        print "AI 1 wins!  Resetting board:";
-        board = [[0 for i in range(0, 3)] for j in range(0, 3)];
-    print "------------"
+    counter = 0;
+    
+    textFont = pygame.font.SysFont("Arial", 14);
+    renderText = lambda x: textFont.render(x, 1, (10, 10, 10));
+    tileColors = [(255, 255, 255), (200, 0, 0), (0, 0, 200), (200, 200, 0)];
+    menuColor = (0, 200, 200);
+    humanColor = (200, 200, 0);
+    while not done:
+        screen.fill((255, 255, 255));
+        
+            
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True;
+            elif USER_INPUT and not USER_CAN_PLAY and event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+                if reset:
+                    board = [[0 for i in range(0, 3)] for j in range(0, 3)];
+                    reset = False;
+                
+                res = aiMakeMove(ais[cnt]);
+                board = res[0];
+                if res[1]:
+                    reset = True;
+                    numWins[cnt] += 1;
+                    ais[cnt].learn(100);
+                    for i in range(0, len(ais)):
+                        if i != cnt:
+                            ais[i].learn(-100);
+                elif isTie(board):
+                    reset = True;
+                    numTies += 1;
+                cnt = (cnt + 1) % len(ais);
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                p = event.pos;
+                if inBounds(p, 400, 200, 150, 50):
+                    USER_INPUT = not USER_INPUT;
+                elif inBounds(p, 600, 200, 150, 50):
+                    board = [[0 for i in range(0, 3)] for j in range(0, 3)];
+                    USER_CAN_PLAY = not USER_CAN_PLAY;
+
+                if USER_CAN_PLAY:
+                    if reset:
+                        board = [[0 for i in range(0, 3)] for j in range(0, 3)];
+                        reset = False;
+                        continue;
+                    c = event.pos[0]/TILE_SIZE;
+                    r = event.pos[1]/TILE_SIZE;
+                    if r < 3 and c < 3 and board[r][c] == 0:
+                        board[r][c] = 2;
+
+                        if getReward(board, 2) > 0:
+                            ais[0].learn(-100);
+                            reset = True;
+                            print "YAY YOU WIN";
+                            continue;
+
+                        if isTie(board):
+                            reset = True;
+                            numTies += 1;
+                        
+                        
+                        res = aiMakeMove(ais[0]);
+                        board = res[0];
+                        if res[1]:
+                            reset = True;
+                            numWins[cnt] += 1;
+                            ais[0].learn(100);
+                            for i in range(0, len(ais)):
+                                if i != cnt:
+                                    ais[i].learn(-100);
+                                    
+                        if isTie(board):
+                            reset = True;
+                            numTies += 1;
+                        
+        
+                    
+        if not USER_INPUT and not USER_CAN_PLAY:
+            if reset:
+                board = [[0 for i in range(0, 3)] for j in range(0, 3)];
+                reset = False;
+            
+            res = aiMakeMove(ais[cnt]);
+            board = res[0];
+            if res[1]:
+                reset = True;
+                numWins[cnt] += 1;
+                ais[cnt].learn(100);
+                for i in range(0, len(ais)):
+                    if i != cnt:
+                        ais[i].learn(-100);
+            elif isTie(board):
+                reset = True;
+                numTies += 1;
+            cnt = (cnt + 1) % len(ais);
+    
+
+        for i in range(0, len(board)):
+            for j in range(0, len(board[i])):
+                rect = pygame.Rect(j*TILE_SIZE, i*TILE_SIZE, TILE_SIZE, TILE_SIZE)
+                pygame.draw.rect(screen, tileColors[board[i][j]], rect);
+        if USER_CAN_PLAY:
+            pos = pygame.mouse.get_pos()
+            c = pos[0]/TILE_SIZE;
+            r = pos[1]/TILE_SIZE;
+            if r < 3 and c < 3 and board[r][c] == 0:
+                rect = pygame.Rect(c*TILE_SIZE, r*TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                pygame.draw.rect(screen, (0, 255, 0), rect);
+        pygame.draw.rect(screen, menuColor, pygame.Rect(400, 200, 150, 50));
+        pygame.draw.rect(screen, menuColor, pygame.Rect(600, 200, 150, 50));
+        screen.blit(renderText("Toggle Speed"), (410, 200));
+        screen.blit(renderText("Face AI 1"), (610, 200));
+        screen.blit(renderText("AI 1 wins: " + str(numWins[0])), (500, 50));
+        screen.blit(renderText("AI 2 wins: " + str(numWins[1])), (500, 100));
+        screen.blit(renderText("Ties: " + str(numTies)), (500, 150));
+        pygame.display.flip();
+    pygame.quit();
+    sys.exit();
+                
+        
